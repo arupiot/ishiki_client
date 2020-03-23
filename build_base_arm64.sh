5723c7c64c77b792951a93fa163d145dd41a6fcf
Paul Harter <paul@glowinthedark.co.uk>
2020-03-23 11:17:46 +0000

    force cache bust for python req

--- build_base_arm64.sh
+++ build_base_arm64.sh
@@ -1,3 +1,3 @@
 
-docker build -f Dockerfile_base_arm64 -t arupiot/ishiki_client_base:arm64 .
+docker build --no-cache -f Dockerfile_base_arm64 -t arupiot/ishiki_client_base:arm64 .
 docker push arupiot/ishiki_client_base:arm64
